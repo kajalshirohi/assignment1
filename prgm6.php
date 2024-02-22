84246233c20efe13219c1bce80cbb62f4c85ceed
kajalshirohi <kajal.shirohi07@gmail.com>
2024-02-22 13:17:14 +0530

    sxth program

---
+++ prgm6.php
@@ -0,0 +1,10 @@
+<?php
+$username="kirtibhardwaj";
+$password="kirti@21";
+if ($username=="kirtibhardwaj" and $password=="kirti@21"){
+    echo "LOGIN SUCCESSFUL";
+}
+else {
+    echo "INVALID CREDENTIALS";
+}
+?>
